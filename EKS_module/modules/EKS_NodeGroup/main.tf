f6549bc638effb353da925797f59ac547a0e36df
Atin Mondal <atincse0@gmail.com>
2023-09-23 01:16:17 +0530

    added eks terraform file in jenkins

--- EKS_module/modules/EKS_NodeGroup/main.tf
+++ EKS_module/modules/EKS_NodeGroup/main.tf
@@ -36,7 +36,7 @@ resource "aws_eks_node_group" "node_group" {
     force_update_version = false
 
 # List of instance types associated with the EKS Node Group
-  instance_types = ["t2.micro"]
+  instance_types = ["t2.medium"]
 
 # Kubernetes version
   #version = "1.24"
